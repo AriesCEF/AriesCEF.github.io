9b91f2f7ae61eb1f85d19b1f7511b2dab963c270
AriesCEF <122508420+AriesCEF@users.noreply.github.com>
2023-01-19 22:24:08 -0300

    validacion del formulario contacto

---
+++ js/validar.js
@@ -0,0 +1,36 @@
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("correo")
+const mensaje = document.getElementById("mensaje")
+const form = document.getElementById("form") 
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length <6){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        form.action = "https://formspree.io/f/mqkoaeav"
+        form.onsubmit()
+        parrafo.innerHTML = "Enviado"
+        nombre.value = ""
+        email.value = ""
+        mensaje.value = ""
+    }
+    document.getElementById("boton").style.backgroundColor = "#2b88bd"
+})
+
+
