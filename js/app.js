714d2d682a4870c2a62ab1be2728b6c363c2b171
AriesCEF <122508420+AriesCEF@users.noreply.github.com>
2023-02-03 00:51:30 -0300

    agrego boton subir arriba

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//ventana Modal del portafolio
 const imagenes = document.querySelectorAll('.galeria .contenedor-imagen');
 const imagenModal = document.getElementById('imagen-modal');
 const parrafoModal= document.getElementById('parrafo-modal');
@@ -10,3 +11,31 @@ imagenes.forEach((imagen)=> {
 		parrafoModal.textContent=ruta1;
 	});
 });
+
+
+//Scroll up
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+	var currentScroll = document.documentElement.scrollTop;
+
+	if(currentScroll > 0){
+		
+		window.scrollTo (0, currentScroll-(currentScroll/0));
+	}
+}
+
+
+window.onscroll = function(){
+
+	var scroll = document.documentElement.scrollTop;
+	
+	if (scroll > 150) {
+		document.getElementById("button-up").style.transform = "scale(1)";
+	} else{
+		if(scroll<150){
+			document.getElementById("button-up").style.transform = "scale(0)";
+		}
+	}
+}
